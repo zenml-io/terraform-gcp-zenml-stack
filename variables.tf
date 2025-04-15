289daf581eb17fec9b01f434319cb8f4462485ea
Stefan Nica <stefan@zenml.io>
2025-04-15 13:46:07 +0200

    Add custom configuration variables for stack components (#4)

--- variables.tf
+++ variables.tf
@@ -9,6 +9,42 @@ variable "orchestrator" {
   }
 }
 
+variable "artifact_store_config" {
+  description = "Additional configuration for the artifact store"
+  type        = map(string)
+  default     = {}
+}
+
+variable "orchestrator_config" {
+  description = "Additional configuration for the orchestrator"
+  type        = map(string)
+  default     = {}
+}
+
+variable "step_operator_config" {
+  description = "Additional configuration for the step operator"
+  type        = map(string)
+  default     = {}
+}
+
+variable "container_registry_config" {
+  description = "Additional configuration for the container registry"
+  type        = map(string)
+  default     = {}
+}
+
+variable "image_builder_config" {
+  description = "Additional configuration for the image builder"
+  type        = map(string)
+  default     = {}
+}
+
+variable "experiment_tracker_config" {
+  description = "Additional configuration for the experiment tracker"
+  type        = map(string)
+  default     = {}
+}
+
 variable "zenml_stack_name" {
   description = "A custom name for the ZenML stack that will be registered with the ZenML server"
   type        = string
